b8a45e9be674f1499027c61c031771d8c7b4eb3f
Rick Chen <rickctchen@google.com>
2020-04-16 09:32:00 +0200

    surya: sensors: Enable multihal 2.0
    Surya requires android.hardware.sensors@2.0-service.multihal
    
    Change-Id: I582b0b224bc507aff426c338f6b7067465fb5c97
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -348,6 +348,9 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal 
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
