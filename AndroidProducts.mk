237dbe22d9f8750dc22a62625468376d3fe032fd
HinohArata <mmgcntk@gmail.com>
2024-04-23 12:45:45 +0700

    surya: Decommonize for Xdroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Xdroid Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/xdroid_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    xdroid_surya-user \
+    xdroid_surya-userdebug \
+    xdroid_surya-eng
--- xdroid_surya.mk
+++ xdroid_surya.mk
@@ -12,12 +12,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xdroid/config/common.mk)
 
-PRODUCT_NAME := lineage_surya
+# Xdroid stuff
+XD_BOOT := 1080
+XDROID_MAINTAINER := Skyy丨私は空です
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_BLUR := true
+
+# Device Manufacture
+PRODUCT_NAME := xdroid_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := POCO/surya_global/surya: 12/RKQ1.211019.001/ V14.0.2.0.SJGMIXM:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="surya_global-user 12 RKQ1.211019.001 V14.0.2.0.SJGMIXM release-keys"
