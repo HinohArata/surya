b70147dad78cb3e796dd0e29af6de4254dc42e77
HinohArata <mmgcntk@gmail.com>
2024-04-23 12:49:49 +0700

    surya: Add Device Product overlay for Xdroid

--- device.mk
+++ device.mk
@@ -348,6 +348,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_surya/com.nxp.mifare.xml
 
 # Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-xdroid
+
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
